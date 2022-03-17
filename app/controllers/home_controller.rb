43d65aa004178b7befc03ac713c45987d448c1e6
Guillermo Moreno <gmq@guillermomoreno.cl>
2022-03-17 11:49:20 -0300

    feat(examples): add base accordion

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
 class HomeController < ApplicationController
-  def index
-  end
+  def index; end
+  def accordion; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get 'accordion', to: 'home#accordion'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   namespace :api, defaults: { format: :json } do
